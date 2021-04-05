7264496a362119ae9663b085c76a069dae650cb6
Dusan Savic <savicd130@gmail.com>
2021-04-06 01:49:29 +0200

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,27 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(el => el._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found' });
-  }
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const port = process.env.PORT || 4000;
+app.use((err, req, res, nest) => {
+  res.status(500).send({ message: err.message });
+});
 
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log('Server at port ' + port);
 });
