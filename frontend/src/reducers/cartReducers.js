adaf37fe142303b73b77395b755d07f9a0f22366
Dusan Savic <savicd130@gmail.com>
2021-04-05 22:44:01 +0200

    cart

---
+++ frontend/src/reducers/cartReducers.js
@@ -0,0 +1,26 @@
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
+
+export const cartReducer = (state = { cartItems: [] }, action) => {
+  switch (action.type) {
+    case CART_ADD_ITEM:
+      const item = action.payload;
+      const existItem = state.cartItems.find(el => item.product === el.product);
+      if (existItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map(el =>
+            el.product === existItem.product ? item : el
+          ),
+        };
+      } else {
+        return { ...state, cartItems: [...state.cartItems, item] };
+      }
+    case CART_REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(el => el.product !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
