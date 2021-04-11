87e5dd0723bdcfcf68b28bff14285c048636ad4f
Dusan Savic <savicd130@gmail.com>
2021-04-11 13:40:44 +0200

    heroku fix

--- backend/server.js
+++ backend/server.js
@@ -29,10 +29,13 @@ app.get('/api/config/paypal', (req, res) => {
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  );
+}
 
 // app.get('/', (req, res) => {
 //   res.send('Server is running');
